f55cfa1cecb29b1f43d5aed8e932ed6e87ce2487
Webstack-Corporation <webstackcorp@gmail.com>
2023-11-20 00:16:06 +0000

    create course page UI

--- app/(dasboard)/layout.tsx
+++ app/(dasboard)/layout.tsx
@@ -1,5 +1,5 @@
-import { Navbar } from "../_components/Navbar";
-import Sidebar from "../_components/Sidebar";
+import { Navbar } from "./_components/Navbar";
+import Sidebar from "./_components/Sidebar";
 
 
 const DashboardLayout = ({children} : {children: React.ReactNode}) => {
@@ -12,7 +12,7 @@ const DashboardLayout = ({children} : {children: React.ReactNode}) => {
             <div className="hidden md:flex flex-col h-full w-56 fixed inset-y-0 z-50">
                 <Sidebar/>
             </div>
-            <main className="md:pl-56 h-full">
+            <main className="md:pl-56 pt-[80px] h-full">
                 {children}
             </main>
       </div>
